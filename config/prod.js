d61ab95a2855bbab26afdaaef09db95567c234b0
rawatmanoj <42403468+rawatmanoj@users.noreply.github.com>
2020-03-05 11:34:13 +0530

    addig

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+
+
+module.exports={
+    googleClientID:process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    cookieKey:process.env.COOKIE_KEY,
+    mongoURI:process.env.MONGO_URI
+}
+
+//mongodb+srv://rawatmanoj997:rawat9997752847@cluster0-uy91z.mongodb.net/test?retryWrites=true&w=majority
--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+require('./models/User');
+require('./services/passport');
+
+
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send({bye:'buddy'});
-});
+app.use(
+    cookieSession({
+        maxAge:30*24*60*60*1000,
+        keys:[keys.cookieKey]
+    }) 
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+
 
 
 const PORT = process.env.PORT || 5000;
